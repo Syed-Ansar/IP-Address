5b24b214e78ca83a4e4f4642441acc05a6ad1ff2
syedansar <syedansar6666@gmail.com>
2021-11-04 16:33:05 +0530

    Favicon

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,24 +9,26 @@ const Card = () => {
       <div className='text-black bg-white w-[310px] md:w-[600px] lg:w-[800px] xl:w-[1150px] rounded-xl p-5 shadow-xl flex flex-col md:flex-row md:py-8 md:pl-2 md:px-6 lg:px-10 justify-between '>
         <div className=' pl-2 px-2'>
           <div className='card md:text-[10px]'>ip address</div>
-          <div className='md:text-[20px]'>{ip}</div>
+          <div className='md:text-[20px]'>{ip ? ip : '8.8.8.8'}</div>
         </div>
         <div className='vl hidden md:inline-block'></div>
         <div className=' pl-1.5 px-2'>
           <div className='card md:text-[10px]'>location</div>
-          <div className='md:text-[20px]'>{location && location.region}</div>
+          <div className='md:text-[20px]'>
+            {location && location.region ? location.region : 'US'}
+          </div>
         </div>
         <div className='vl hidden md:inline-block'></div>
         <div className='pl-1.5 px-2'>
           <div className='card md:text-[10px]'>timezone</div>
           <div className='md:text-[20px]'>
-            {(location && location.timezone) || ' '}
+            {location && location.timezone ? location.timezone : '-07:00'}
           </div>
         </div>
         <div className='vl hidden md:inline-block'></div>
         <div className='pl-1.5 px-2'>
           <div className='card md:text-[10px]'>isp</div>
-          <div className='md:text-[20px]'>{isp}</div>
+          <div className='md:text-[20px]'>{isp ? isp : 'Google LLC'}</div>
         </div>
       </div>
     </div>
